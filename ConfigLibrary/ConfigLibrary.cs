d34aa173a336b264b22420cbc22ce7f0c9ea0bb0
cmrak@lus.org <cmrak@lus.org>
2019-01-11 16:07:36 -0600

    initial

---
+++ ConfigLibrary/ConfigLibrary.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+
+namespace ConfigLibrary
+{
+    public class ConfigLibrary
+    {
+        string ReturnKeyValue(string key)
+        {
+            if (key is string)
+            {
+                string configValue = ConfigurationManager.AppSettings[key];
+
+                return configValue;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
